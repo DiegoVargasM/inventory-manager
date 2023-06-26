04cbebb23058dca23668625892fef650cf0e5ef7
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-06-26 13:40:17 -0500

    Add middleware and route test

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,23 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 //converts the data to json
 const bodyParser = require("body-parser");
+const e = require("cors");
 
 const app = express();
 
 const PORT = process.env.PORT;
 
-//Connect to DB & start server
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+//Route
+app.get("/", (req, res) => {
+  res.send({ message: "API Working" });
+});
+
+// Connect to DB & start server
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
