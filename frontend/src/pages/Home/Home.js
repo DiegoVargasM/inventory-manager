fa5f351c8f1d2c298d165e44f5cc1a109bdb845c
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-06-30 12:42:27 -0500

    Add home page and style it

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,5 +1,67 @@
+import React from "react";
+import { RiProductHuntLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
+import "./Home.scss";
+import invImg from "../../assets/inv-img.jpg";
+
 const Home = () => {
-  return <div>Home, hello world</div>;
+  return (
+    <div className="home">
+      <nav className="container --flex-between ">
+        <div className="logo">
+          <RiProductHuntLine size={35} />
+        </div>
+
+        <ul className="home-links">
+          <li>
+            <button className="--btn --btn-primary">
+              <Link to="/register">Register</Link>
+            </button>
+          </li>
+
+          <li>
+            <button className="--btn --btn-primary">
+              <Link to="/log-in">Login</Link>
+            </button>
+          </li>
+
+          <li>
+            <button className="--btn --btn-primary">
+              <Link to="/dashboard">Dashboard</Link>
+            </button>
+          </li>
+        </ul>
+      </nav>
+      <section className="container hero">
+        <div className="hero-text">
+          <h2>Warehouse Stock Management App</h2>
+          <p>
+            Inventory system to control and manage warehouse products in real
+            time to make it easier to develop your business.
+          </p>
+          <div className="--flex-start">
+            <NumberText num="5000+" text="Brand Owners" />
+            <NumberText num="23K+" text="Active Users" />
+            <NumberText num="750+" text="Partners" />
+          </div>
+        </div>
+
+        <div className="hero-image">
+          <img src={invImg} alt="Stock_Image" />
+        </div>
+      </section>
+      <section>Carousel</section>
+    </div>
+  );
+};
+
+const NumberText = ({ num, text }) => {
+  return (
+    <div className="--mr">
+      <h3 className="--color-white">{num}</h3>
+      <p className="--color-white">{text}</p>
+    </div>
+  );
 };
 
 export default Home;
