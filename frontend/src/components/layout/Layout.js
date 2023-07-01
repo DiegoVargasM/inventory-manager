b76183ab9ed810603da294fcab750832f15535a6
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-07-01 09:31:18 -0500

    Fix typo error

--- frontend/src/components/layout/Layout.js
+++ frontend/src/components/layout/Layout.js
@@ -1,13 +1,12 @@
-import React, { Children } from "react";
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
-import styles from "./Layout.module.css";
+import styles from "./Layout.module.scss";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
-      <div style={styles.layout}>{children}</div>
+      <div className={styles.layout}>{children}</div>
       <Footer />
     </>
   );
