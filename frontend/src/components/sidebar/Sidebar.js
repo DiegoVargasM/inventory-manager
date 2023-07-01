451277ae0775eb08d992c9be89c9b8d3c0972c4b
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-07-01 09:21:04 -0500

    Initialize sidebar comp

---
+++ frontend/src/components/sidebar/Sidebar.js
@@ -0,0 +1,14 @@
+import "./Sidebar.scss";
+
+const Sidebar = ({ children }) => {
+  return (
+    <div className="layout">
+      <div className="sidebar">
+        <h2>Sidebar</h2>
+      </div>
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default Sidebar;
