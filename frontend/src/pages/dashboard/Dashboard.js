cec8b619c5aee887417a83a642fac6253fc291c4
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-07-03 16:54:14 -0500

    Create Dashboard

--- frontend/src/pages/dashboard/Dashboard.js
+++ frontend/src/pages/dashboard/Dashboard.js
@@ -1,10 +1,35 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ProductList from "../../components/product/productList/ProductList";
+import ProductSummary from "../../components/product/productSummary/productSummary";
 import useRedirectLoggedOutUser from "../../customHooks/useRedirectLoggedOut";
+import { selectIsLoggedIn } from "../../redux/features/auth/authSlice";
+import { getProducts } from "../../redux/features/products/productSlice";
 
 const Dashboard = () => {
   useRedirectLoggedOutUser("/log-in");
+  const dispatch = useDispatch();
+
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  // we want to bring the following from the productSlice:
+  const { products, isLoading, isError, message } = useSelector(
+    (state) => state.product
+  );
+
+  useEffect(() => {
+    if (isLoggedIn === true) {
+      dispatch(getProducts());
+    }
+
+    if (isError) {
+      console.log(message);
+    }
+  }, [isLoggedIn, isError, message, dispatch]);
+
   return (
     <div>
-      <h2>Dashboard</h2>
+      <ProductSummary products={products} />
+      <ProductList products={products} isLoading={isLoading} />
     </div>
   );
 };
